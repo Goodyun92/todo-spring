282b3208b5d3ae5dbecae86f69902326bae1df90
dongyun <0902hdy@gmail.com>
2023-09-15 18:05:28 +0900

    update swagger https

--- src/main/java/com/example/todo/ToDoApplication.java
+++ src/main/java/com/example/todo/ToDoApplication.java
@@ -1,8 +1,11 @@
 package com.example.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://www.mytodo.shop", description = "Default Server URL")})
 @SpringBootApplication
 public class ToDoApplication {
 
