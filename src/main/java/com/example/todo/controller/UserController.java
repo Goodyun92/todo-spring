049f0fb7ceebca44499b169ed0a98d1f800b05c5
dongyun <0902hdy@gmail.com>
2023-09-15 14:38:00 +0900

    add swagger

--- src/main/java/com/example/todo/controller/UserController.java
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -3,6 +3,12 @@ package com.example.todo.controller;
 import com.example.todo.model.User;
 import com.example.todo.model.dto.UserDto;
 import com.example.todo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "users", description = "users API Document")
 public class UserController {
 
 //    @Autowired
@@ -20,24 +27,27 @@ public class UserController {
 
     @CrossOrigin("*")
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입", description = "회원가입을 수행합니다.")
     public ResponseEntity<User> signUp(@RequestBody UserDto userDto) {
         User savedUser = userService.signUp(userDto);
         return ResponseEntity.ok(savedUser);
     }
 
     @CrossOrigin("*")
+    @PostMapping("/{user_id}/log-out")
+    @Operation(summary = "로그아웃", description = "로그아웃을 수행합니다.")
+    public ResponseEntity<Void> logOut(@PathVariable("user_id") String userId) {
+        userService.logOut(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @CrossOrigin("*")
     @GetMapping("/log-in")
+    @Operation(summary = "로그인", description = "로그인을 수행합니다.")
     public ResponseEntity<Long> logIn(@RequestParam String userName, @RequestParam String password) {
         Optional<Long> userId = userService.logIn(userName, password);
         return userId.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.badRequest().build());
-    }
-
-    @CrossOrigin("*")
-    @PostMapping("/{user_id}/log-out")
-    public ResponseEntity<Void> logOut(@PathVariable("user_id") String userId) {
-        userService.logOut(userId);
-        return ResponseEntity.ok().build();
     }
 }
 
